add08791e6dfa1b37f67e33d848e3f7aeed2d2fc
tinhngo-saigontechnology <tinh.ngo@saigontechnology.com>
2024-01-29 14:11:31 +0700

    feat: update new features for version 2.3.0

--- SampleWebsiteBlazorWasm/Shared/AcquireEventModel.cs
+++ SampleWebsiteBlazorWasm/Shared/AcquireEventModel.cs
@@ -4,5 +4,6 @@
     {
         public string AcquireResponse { get; set; } = string.Empty;
         public string AcquireError { get; set; } = string.Empty;
+        public bool IsUpload { get; set; } = true;
     }    
 }
--- SampleWebsiteBlazorWasm/Shared/TwainOptionsModel.cs
+++ SampleWebsiteBlazorWasm/Shared/TwainOptionsModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SampleWebsiteBlazorWasm.Shared
+﻿namespace SampleWebsiteBlazorWasm.Shared
 {
     public class TwainOptionsModel
     {
@@ -11,5 +6,6 @@ namespace SampleWebsiteBlazorWasm.Shared
         public OcrTypeModel OcrType { get; set; }
         public PagePlacementModel PagePlacement { get; set; }
         public OutputFileTypeModel OutputFileType { get; set; }
+        public SaveToTypeModel SaveToType { get; set; }
     }
 }
