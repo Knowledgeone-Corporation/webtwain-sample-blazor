28fabba8f4d7c070b6127995f37f4274738f567b
tinhngo-saigontechnology <tinh.ngo@saigontechnology.com>
2025-08-21 15:09:46 +0700

    feat: Options to allow compression scanned images

---
+++ SampleWebsiteBlazorWasm/Shared/Models/FileCompressionTypeModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SampleWebsiteBlazorWasm.Shared.Models
+{
+    public class FileCompressionTypeModel
+    {
+        public int None { get; } = 0;
+        public int Low { get; } = 1;
+        public int Medium { get; } = 2;
+        public int High { get; } = 3;
+
+        public Dictionary<string, int> FileCompressionTypes => new Dictionary<string, int>()
+        {
+            { nameof(None), None },
+            { nameof(Low), Low },
+            { nameof(Medium), Medium },
+            { nameof(High), High }
+        };
+    }
+}
